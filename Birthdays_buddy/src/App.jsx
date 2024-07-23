a81c3b1da6e50b3e887f4ce03f5a8e78287bcadc
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-07-23 20:37:49 +0000

    code style and clear all birthdays_buddy

--- Birthdays_buddy/src/App.jsx
+++ Birthdays_buddy/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import data from "./data"
-
+import "./myCSS.css"
 const App = () => {
   const [friends,setClearAll] = useState(data)
 
@@ -8,21 +8,30 @@ const App = () => {
     setClearAll([])
   };
   return <>
-    <div>
-      <h2>Birthday Reminder - Starter</h2>
+    <main className="mainContent">
+      <section>
+        <h2>Birthday Reminder - Starter</h2>
+        <List friends={friends} />
+        <button className="btn btn-width-full" onClick={handleClickClearAll}>Clear All</button>
+      </section>
+    </main>
+  </>
+};
+const List = ({friends}) => {
+  return (
       <ul>
         {
           friends.map((friend) => {
             return <li key={friend.id}>
-              <img src={friend.image} alt="avatar" />
-              <h4>{friend.name}</h4>
-              <h5>{friend.age}</h5>
+              <img src={friend.image} alt="avatar" style={{width:"80px"}}/>
+              <div>
+                <h4>{friend.name}</h4>
+                <h5>{friend.age} years</h5>
+              </div>
             </li>
           })
         }
-      </ul>
-      <button className="btn" onClick={handleClickClearAll}>Clear All</button>
-    </div>
-  </>
-};
+      </ul>)
+  
+}
 export default App;
