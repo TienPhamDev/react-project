6a9661504b8050a757ec7159f77bf57587de91c6
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-07-23 18:45:48 +0000

    code clear all button

--- Birthdays_buddy/src/App.jsx
+++ Birthdays_buddy/src/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import data from "./data"
 
 const App = () => {
-  const friends = data;
+  const [friends,setClearAll] = useState(data)
+
+  const handleClickClearAll = () =>{
+    setClearAll([])
+  };
   return <>
     <div>
-      <h2>Birthday Reminder - Starter</h2>;
+      <h2>Birthday Reminder - Starter</h2>
       <ul>
         {
           friends.map((friend) => {
-            return <li>
+            return <li key={friend.id}>
               <img src={friend.image} alt="avatar" />
               <h4>{friend.name}</h4>
               <h5>{friend.age}</h5>
@@ -16,7 +21,7 @@ const App = () => {
           })
         }
       </ul>
-      <button>Clear All</button>
+      <button className="btn" onClick={handleClickClearAll}>Clear All</button>
     </div>
   </>
 };
