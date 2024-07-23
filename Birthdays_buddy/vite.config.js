782f85047b4dc8018d7a8083231eb3c803636395
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-07-23 23:23:30 +0000

    cm

--- Birthdays_buddy/vite.config.js
+++ Birthdays_buddy/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Birthdays_buddy/",
+  base:"/Birthday_buddy/",
 })
