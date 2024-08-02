feac6985a2881a25fae20672935a9ae787b89a6b
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-08-01 22:51:30 -0400

    code style tailwind header

--- tienPhamPortfoio/src/App.jsx
+++ tienPhamPortfoio/src/App.jsx
@@ -7,12 +7,13 @@ function App() {
   )
 };
 function Header(){
-  const styleHeaderEl = "fixed top-0 left-0 right-0 h-16 bg-slate-100 flex items-center justify-between px-4 py-8 shadow-lg"
-  const styleLogo = "text-cyan-700 font-bold"
+  const styleHeaderEl = "mx-auto lg:px-64 fixed top-0 left-0 right-0 h-16 bg-slate-100 flex items-center justify-between px-4 py-8 shadow-lg"
+  const styleLogo = "text-cyan-700 font-bold md:order-first"
 
   //state 
   const [mode,setMode] = useState(false)
-
+  const [menuIsOpen,setMenuIsOpen] = useState(false)
+  // dark mode toggle button
   function handleMode(){
     setMode(!mode)
     const modeButton = document.querySelector("#modeButton")
@@ -34,14 +35,12 @@ function Header(){
     } 
     
   }
-  const [menuIsOpen,setMenuIsOpen] = useState(false)
   function handleMenuIsOpen(){
     setMenuIsOpen(!menuIsOpen)
   }
   return <header className={styleHeaderEl}>
     <a className={styleLogo} href="#"><span>{"< TPDev />"}</span></a>
-    <button id="modeButton" onClick={handleMode}>
-      
+    <button id="modeButton" onClick={handleMode} className="md:order-last ">  
         <svg   xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" 
             className="size-6">
           <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
@@ -51,12 +50,9 @@ function Header(){
             className="size-6">
           <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
         </svg>
-
-      
-        
     </button>
-    <nav className="">
-      <button onClick={handleMenuIsOpen}>
+    <nav className="order-last md:order-3">
+      <button onClick={handleMenuIsOpen} className="md:hidden">
         { !menuIsOpen ?
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#0e7490" 
             className="size-6 transition duration-300">
@@ -69,7 +65,7 @@ function Header(){
         </svg>
         }
       </button>
-      <ul className={menuIsOpen || "hidden"} id="menu">
+      <ul className={menuIsOpen || "hidden md:block"} id="menu">
         <li><a href="#">Home</a></li>
         <li><a href="#">Portfolio</a></li>
         <li><a href="#">Blog</a></li>
