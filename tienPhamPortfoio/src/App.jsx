16f97067bdf1d510265a4f1233432ee60974e485
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-08-01 21:56:29 -0400

    code header dark mode button

--- tienPhamPortfoio/src/App.jsx
+++ tienPhamPortfoio/src/App.jsx
@@ -15,27 +15,24 @@ function Header(){
 
   function handleMode(){
     setMode(!mode)
-    const moonSvg = document.querySelector("#moonSvg")
-    const sunSvg = document.querySelector("#sunSvg")
     const modeButton = document.querySelector("#modeButton")
-    if(mode){
-      modeButton.children[1].style.left = "-50%";
-      sunSvg.style.opacity = "1";
-      sunSvg.style.transform ="scale(1)";
-      moonSvg.style.opacity = "0";
-      moonSvg.style.transition = "all 0.3s ease-out";
-      moonSvg.style.transform = "scale(0)";
-      moonSvg.style.zIndex = "-10";
-    } 
     if (!mode){
-      modeButton.firstElementChild.style.left = "0";
-      moonSvg.style.opacity = "1";
-      moonSvg.style.transform ="scale(1)";
-      sunSvg.style.opacity = "0";
-      sunSvg.style.transition = "all 0.3s ease-out";
-      sunSvg.style.transform = "scale(0)";
-      sunSvg.style.zIndex = "-10";
+      modeButton.style.backgroundColor = "var(--color-slate-900)"
+      modeButton.lastElementChild.style.opacity = "0";
+      modeButton.lastElementChild.style.right = "5%";
+      modeButton.children[1].style.left = "75%";
+      modeButton.firstElementChild.style.left = "25%";
+      modeButton.firstElementChild.style.opacity = "1";
     }
+    if(mode){
+      modeButton.style.backgroundColor = "var(--color-slate-200)"
+      modeButton.firstElementChild.style.opacity = "0";
+      modeButton.firstElementChild.style.left = "5%";
+      modeButton.children[1].style.left = "25%";
+      modeButton.lastElementChild.style.right = "25%";
+      modeButton.lastElementChild.style.opacity = "1";
+    } 
+    
   }
   const [menuIsOpen,setMenuIsOpen] = useState(false)
   function handleMenuIsOpen(){
@@ -45,12 +42,12 @@ function Header(){
     <a className={styleLogo} href="#"><span>{"< TPDev />"}</span></a>
     <button id="modeButton" onClick={handleMode}>
       
-        <svg id="moonSvg"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" 
+        <svg   xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" 
             className="size-6">
           <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
         </svg>
-        <span></span>
-        <svg id="sunSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" 
+        <span ></span>
+        <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" 
             className="size-6">
           <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
         </svg>
