72275db419681660e99cff5e1d90fabebec0df7e
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-08-20 12:03:32 -0400

    code section hero responsive

--- tienPhamPortfoio/src/components/SectionHero.jsx
+++ tienPhamPortfoio/src/components/SectionHero.jsx
@@ -3,20 +3,21 @@ import "../myCss/sectionHero.css"
 
 export const SectionHero = ({theme}) => {
   const styleSectionHero = theme === "dark" ? "dark-bg-image-gradient" : "bg-image-gradient"
-  const styleDivImg = "relative after:content-[''] after:left-[8%] after:rounded-2xl after:border-b-4 dark:after:border-cyan-600 after:border-r-4 after:border-cyan-900 after:w-full after:h-full after:bottom-[-8%] after:absolute ";
+  const styleDivImg = "md:row-start-1 md:col-start-2 relative after:content-[''] after:left-[8%] after:rounded-2xl after:w-40 after:h-40 after:border-b-4 dark:after:border-cyan-600 after:border-r-4 after:border-cyan-900 after:bottom-[-8%] after:absolute ";
   const styleImg = "z-10 h-40 w-40 rounded-2xl border-4 dark:border-cyan-300 border-cyan-600";
-  const styleH1 = "dark:text-slate-100 text-5xl text-slate-800 text-center leading-tight tracking-wide";
+  const styleDivHero = "md:col-start-1"
+  const styleH1 = "md:text-left md:p-2 dark:text-slate-100 text-5xl text-slate-800 text-center leading-tight tracking-wide";
   const styleHello = "text-6xl text-inherit font-bold bg-inherit";
   const styleNameMark = "bg-inherit text-cyan-600 font-bold";
-  const styleP = "p-4 dark:text-slate-100 text-center text-lg mt-4 tracking-wide text-slate-600";
+  const styleP = "md:text-left md:p-2 p-4 dark:text-slate-100 text-center text-lg mt-4 tracking-wide text-slate-600";
   const styleLocationText = "dark:text-slate-200 bg-inherit font-semibold text-slate-800"
   const styleArrowDownSVG = "size-10"
-  const styleDivArrow = "relative gap-0 mb-12"
+  const styleDivArrow = "md:col-span-2 relative gap-0 mb-12"
   return <section id="hero" className={`sectionHero ${styleSectionHero}`}>
     <div className={styleDivImg}>
       <img src={tienAvatar} alt="Tien avatar" className={styleImg} />
     </div>
-    <div>
+    <div className={styleDivHero}>
       <h1 className={styleH1}><mark className={styleHello}>Hello</mark>, <br />I am <mark className={styleNameMark}>Tien Pham</mark>, Frontend Developer</h1>
       <p className={styleP}>Based in <mark className={styleLocationText}>West Melbourne, Florida,</mark> I'm passionate about creating engaging, digital experiences on the web.</p>
     </div>
