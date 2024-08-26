479ff909e306976ab09fd4eaf9ad35a7c76d4250
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-08-26 12:01:05 -0400

    code project

--- tienPhamPortfoio/src/components/MainPage.jsx
+++ tienPhamPortfoio/src/components/MainPage.jsx
@@ -10,3 +10,14 @@ export const MainPage = ({theme}) => {
     </main>
   )
 };
+const SectionContact = () => {
+  return (
+    <section>
+      <h2>Get In Touch</h2>
+      <p>Interested in working with me or hiring me for your next project? Drop me a line and let's make it happen.
+        <br />
+        Get in touch today and let's bring your ideas to life.
+      </p>
+    </section>
+  )
+}
