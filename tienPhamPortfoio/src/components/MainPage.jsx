db59c9e852cf38a1da9c2bdeb315a1714ec83170
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-08-26 14:28:51 -0400

    code css form section contact

--- tienPhamPortfoio/src/components/MainPage.jsx
+++ tienPhamPortfoio/src/components/MainPage.jsx
@@ -1,6 +1,7 @@
 import { SectionHero } from "./SectionHero";
 import { SectionSkill } from "./SectionSkill";
 import { SectionProjects } from "./SectionProjects";
+import { SectionContact } from "./SectionContact";
 export const MainPage = ({theme}) => {
   return (
     <main>
@@ -11,14 +12,3 @@ export const MainPage = ({theme}) => {
     </main>
   )
 };
-const SectionContact = () => {
-  return (
-    <section>
-      <h2>Get In Touch</h2>
-      <p>Interested in working with me or hiring me for your next project? Drop me a line and let's make it happen.
-        <br />
-        Get in touch today and let's bring your ideas to life.
-      </p>
-    </section>
-  )
-}
---
+++ tienPhamPortfoio/src/components/SectionContact.jsx
@@ -0,0 +1,20 @@
+import "../myCss/sectionContact.css"
+
+export const SectionContact = () => {
+  return (
+    <section className="sectionContact">
+      <h2>Get In Touch</h2>
+      <p>Interested in working with me or hiring me for your next project? Drop me a line and let's make it happen.
+        <br />
+        <br />
+        Get in touch today and let's bring your ideas to life.
+      </p>
+      <form>
+        <input type="text" id="name" placeholder="Name" />
+        <input type="email" id="email" placeholder="Email" />
+        <textarea name="message" id="message">Your message</textarea>
+        <button type="submit">Send</button>
+      </form>
+    </section>
+  );
+};
