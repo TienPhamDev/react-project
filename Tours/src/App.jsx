d24ee522a5e9e51ed981c89c148fdec1b57f9475
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-09-12 21:17:50 -0400

    app

--- Tours/src/App.jsx
+++ Tours/src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
 import Tours from "./Tours";
 
 const url = 'https://www.course-api.com/react-tours-project';
 
 const App = () => {
+  const [isLoading,setIsLoading] = useState(true);
+  const [toursData,setToursData] = useState([]);
+
+  const fetchToursData = async () => {
+    setIsLoading(true)
+    try {
+      const res = await fetch(url)
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false)
+  }
+  useState(()=>{
+    fetchToursData();
+  },[])
   return (
     <main>
       <h2>Our Tours</h2>
