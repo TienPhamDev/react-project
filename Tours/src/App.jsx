a23544626fe10e4206ac9d3bacd9b6dbe71cc8b7
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-09-12 22:42:01 -0400

    code again tours

--- Tours/src/App.jsx
+++ Tours/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import Tours from "./Tours";
+import Loading from "./Loading";
 
 const url = 'https://www.course-api.com/react-tours-project';
 
 const App = () => {
   const [isLoading,setIsLoading] = useState(true);
   const [toursData,setToursData] = useState([]);
-
+  
+  const removeTour = (id) => {
+    const newTour = toursData.filter((tour) => tour.id !== id)
+    setToursData(newTour)
+  }
   const fetchToursData = async () => {
     setIsLoading(true)
     try {
@@ -21,10 +26,23 @@ const App = () => {
   useState(()=>{
     fetchToursData();
   },[])
+  if(isLoading){
+    return (
+      <Loading />
+    )
+  }
+  if(toursData.length === 0){
+    return (
+      <main>
+        <h2>No more Tour</h2>
+        <button type="button" className="btn" onClick={()=>fetchToursData()}>Refresh</button>
+      </main>
+    )
+  }
   return (
     <main>
       <h2>Our Tours</h2>
-      <Tours tours = {toursData}/>
+      <Tours tours = {toursData} removeTour={removeTour}/>
     </main>
   )
 };
--- Tours/src/Tour.jsx
+++ Tours/src/Tour.jsx
@@ -1,10 +1,20 @@
-const Tour = ({id,name,info,image,price}) => {
+import { useState } from "react";
+
+const Tour = ({id,name,info,image,price,removeTour}) => {
+    const [readmore,setReadmore] = useState(false);
+
     return (
         <div className="single-tour">
-            <img  src={image} alt="tours image" />
+            <img className="img" src={image} alt="tours image" />
             <span className="tour-price">{price}</span>
-            <h2>{name}</h2>
-            <p>{info}</p>
+            <div className="tour-info">
+                <h5>{name}</h5>
+                <p>{readmore ? `${info} ` : `${info.substring(0,200)} ... `} 
+                    <button onClick={()=> setReadmore(!readmore)} type="button" className="info-btn">{readmore ? "Show less" : "Read more"}</button>
+                </p>
+                
+                <button onClick={() => {removeTour(id)}} type="button" className="btn btn-block delete-btn" >Not Interested</button>
+            </div>
         </div>
     );
 };
