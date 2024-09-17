5bccf3eeecc9374e8d56315447decaaf0361604e
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-09-17 10:28:55 -0400

    cm

--- result_summary_component/src/App.jsx
+++ result_summary_component/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import "./css/styles.css";
 import { Result } from './Result';
-
+import { Summary } from './Summary';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main>
       <Result/>
+      <Summary/>
     </main>
   )
 }
 export default App;
+ 
---
+++ result_summary_component/src/Summary.jsx
@@ -0,0 +1,9 @@
+export const Summary = () => {
+  return <div className="summary">
+    <h2>Summary</h2>
+    <div className="categories">
+      
+
+    </div>
+  </div>;
+};
