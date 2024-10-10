e386277d7b918dfab370544ba42831d0ff0c4844
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-10-10 14:21:33 -0400

    code

--- quiz_app/src/header.ts
+++ quiz_app/src/header.ts
@@ -1,3 +1,4 @@
+import modeBtn from "./modeBtn";
 export const header = (element: HTMLElement) :void=>{
     element.innerHTML = `<span class="tittle-quizz">
       </span>
@@ -9,14 +10,5 @@ export const header = (element: HTMLElement) :void=>{
         </button>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path fill="currentColor" d="M11.775 4.522A7.5 7.5 0 1 1 4.898 16.09c2.104-.57 4.974-1.953 6.24-5.326.828-2.211.876-4.408.637-6.241ZM20.184 12a8.997 8.997 0 0 0-9.315-8.994.75.75 0 0 0-.713.888c.345 1.821.42 4.092-.424 6.342-1.2 3.201-4.203 4.26-6.115 4.606a.75.75 0 0 0-.542 1.066A9 9 0 0 0 20.184 12Z"/></svg>
       </span>` 
-    const modeBtn = document.querySelector<HTMLButtonElement>(".modeBtn")!;
-    
-    modeBtn.addEventListener("click",()=>{
-      const body = document.querySelector<HTMLBodyElement>("body")!
-      body.classList.toggle("light");
-      modeBtn.classList.toggle("justify-start")
-      body.classList.toggle("dark");
-      modeBtn.classList.toggle("justify-end")
-      
-    })
+    modeBtn(document.querySelector<HTMLButtonElement>(".modeBtn")!);
     }
