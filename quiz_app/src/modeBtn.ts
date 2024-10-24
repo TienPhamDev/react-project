a6aeb1917f818548a7ff6bb11b5a6ab5637ae69d
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-10-24 10:16:02 +0700

    cm

--- quiz_app/src/modeBtn.ts
+++
@@ -1,9 +0,0 @@
-export default function modeBtn(element:HTMLButtonElement):void{
-    element.addEventListener("click",()=>{
-        const body = document.querySelector<HTMLBodyElement>("body")!
-        body.classList.toggle("light");
-        element.classList.toggle("justify-start")
-        body.classList.toggle("dark");
-        element.classList.toggle("justify-end")    
-      })
-}
